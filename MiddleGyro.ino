5002036aad3a535c45d866c1390302ff110b3804
MarieJulou <64188927+MarieJulou@users.noreply.github.com>
2022-01-13 12:21:24 +0100

    Update MiddleGyro.ino

--- MiddleGyro.ino
+++ MiddleGyro.ino
@@ -2,51 +2,35 @@
 
 // Please include ArduinoOSCWiFi.h to use ArduinoOSC on the platform
 // which can use both WiFi and Ethernet
-
 #include <ArduinoOSCWiFi.h>
+
 // including libraries for the gyrosensor lsm303lhc
-#include <Wire.h>
+
 #include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
+#include <Adafruit_LSM303_Accel.h>
+#include <Wire.h>
 // this is also valid for other platforms which can use only WiFi
-// #include <ArduinoOSC.h>
+
 
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 
 // WiFi stuff
-const char* ssid = "Bienvenue2";
-const char* pwd = "olju1507";
-const IPAddress ip(192, 168, 1, 201);
-const IPAddress gateway(192, 168, 1, 1);
+const char* ssid = "PoleDeVinci_DVIC";
+const char* pwd = "8PfURsp!dvic";
+const IPAddress ip(172, 21, 72, 201);
+const IPAddress gateway(172, 21, 72, 1);
 const IPAddress subnet(255, 255, 255, 0);
 
 // for ArduinoOSC
-const char* host = "192.168.1.94";
+const char* host = "172.21.72.115";
 const int recv_port = 8000;
 const int send_port = 9000;
 // send / receive varibales
 int i;
 float f;
 String s;
-
-void onOscReceived(const OscMessage& m) {
-    Serial.print(m.remoteIP());
-    Serial.print(" ");
-    Serial.print(m.remotePort());
-    Serial.print(" ");
-    Serial.print(m.size());
-    Serial.print(" ");
-    Serial.print(m.address());
-    Serial.print(" ");
-    Serial.print(m.arg<int>(0));
-    Serial.print(" ");
-    Serial.print(m.arg<float>(1));
-    Serial.print(" ");
-    Serial.print(m.arg<String>(2));
-    Serial.println();
-}
 
 void setup() {
     Serial.begin(115200);
@@ -70,7 +54,6 @@ void setup() {
 #ifndef ESP8266
     while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
 #endif
-    Serial.begin(115200);
     Serial.println("Accelerometer Test"); Serial.println("");
 
     /* Initialise the sensor */
@@ -79,6 +62,10 @@ void setup() {
       /* There was a problem detecting the ADXL345 ... check your connections */
       Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
       while(1);
+    }
+    else
+    {
+      Serial.println("LSM303 detected! Let's send it!");
     }
 
     // subscribe osc messages
